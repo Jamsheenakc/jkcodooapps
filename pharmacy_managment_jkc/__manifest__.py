619100139e2a86a23738f075ccd2571060e0d0ba
jamsheenakc <jamsheena384@gmail.com>
2025-05-31 23:57:54 +0400

    18 migration

--- pharmacy_managment_jkc/__manifest__.py
+++ pharmacy_managment_jkc/__manifest__.py
@@ -15,7 +15,7 @@
     'author': "Jamsheena KC",
 
     'category': 'Medical',
-    'version': '17.0.1.0.0',
+    'version': '18.0.1.0.0',
 
     # any module necessary for this one to work correctly
     'depends': ['hr_contract', 'hr', 'hr_payroll', 'stock', 'account', 'purchase', 'sale_management', 'mail'],
